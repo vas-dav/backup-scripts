0d77c75c381099287ca9f6d53ca54c570fe72319
vas-dav <vasilydavydovmail@yahoo.com>
2023-09-27 01:57:01 +0300

    backup_tar: simplify duplication handling

--- backup_tar.sh
+++ backup_tar.sh
@@ -9,27 +9,13 @@ set -xe
 
 out=${1}
 inp=${2}
-ts=$(date "+%Y-%m-%d-%H:%M:%S")
+ts=$(date "+%Y-%m-%d")
 dirname=$(basename $inp)
 
 mkdir -p ${out}/${dirname}_backups/
 tar -zcf ${out}/${dirname}_backups/${ts}_${dirname}.tar.gz ${inp}
 
 cd ${out}/${dirname}_backups/
-array=()
-IFS=$'\n' read -r -d '' -a array < <( ls -1t | head -n +2 && printf '\0' )
-num_files=${#array[@]}
-
-if [ $num_files -ge 2 ]; then
-  hash1=$(md5sum "${array[0]}" | awk '{print $1}')
-  hash2=$(md5sum "${array[1]}" | awk '{print $1}')
-
-  if [ "$hash1" == "$hash2" ]; then
-    rm "${array[1]}"
-    cd -
-    exit 0
-  fi
-fi
 ls -1t | tail -n +21 | xargs rm -f
 cd -
 exit 0
